17d086cd36d5de3ba8202301c99a8491c671f12a
mileeena <tgbgyukjh-fsdfsds@yahoo.com>
2021-12-13 00:25:25 +1000

    правки

--- Data/Repository.cs
+++ Data/Repository.cs
@@ -14,26 +14,31 @@ namespace Phone_book.Data
         {
             List<Сontact> Contacts = new List<Сontact>();
             Сontact temp;
-            //localhost;
-            //user id = root; port = 3306; persistsecurityinfo = False; database = pb_db
-
-            //string connectStr= "server=localhost;user=root;port=3306;database = pb_db;password=Sa25092002;";
 
             MySqlConnectionStringBuilder strConnection = new MySqlConnectionStringBuilder()
             {
-                Server = "localhost",
-                UserID = "root",
+                //для локального сервера
+                //Server = "localhost",
+                //UserID = "root",
+                //Port = 3306,
+                //Database = "pb_db",
+                //Password = "Sa25092002"
+
+                //для сервера на https://www.db4free.net/
+                Server = "db4free.net",
+                UserID = "root_55",
                 Port = 3306,
-                Database = "pb_db",
-                Password = "Sa25092002"
+                Database = "db_contacts_mysq",
+                Password = "qwer1234"
             };
             try
             {
                 MySqlConnection db = new MySqlConnection(strConnection.ConnectionString);
                 db.Open();
-                    var SelectAll = @"SELECT * FROM pb_db.contacts;";
+                    //var SelectAll = @"SELECT * FROM pb_db.contacts;";
+                    var SelectAll = @"SELECT * FROM db_contacts_mysq.contacts;";
 
-                    MySqlCommand selectAllCommand = new MySqlCommand(SelectAll, db);
+                MySqlCommand selectAllCommand = new MySqlCommand(SelectAll, db);
                     MySqlDataReader reader = selectAllCommand.ExecuteReader();
 
                     while (reader.Read())
@@ -54,28 +59,6 @@ namespace Phone_book.Data
             {
 
             }
-
-
-            //return new Сontact()
-            //{
-            //    Id = 1,
-            //    Surname = "surname",
-            //    Name = "name",
-            //    Patronymic = "Витальевич",
-            //    PhoneNumber = "79990863742",
-            //    Address = "KnA"
-            //};
-
-            // Сontact(int Id, string Surname, string Name, string Patronymic, string PhoneNumber, string Address)
-            //{
-            //    this.Id = Id;
-            //    this.Surname = Surname;
-            //    this.Name = Name;
-            //    this.Patronymic = Patronymic;
-            //    this.PhoneNumber = PhoneNumber;
-            //    this.Address = Address;
-
-            //}
             return Contacts;
         }
     }
