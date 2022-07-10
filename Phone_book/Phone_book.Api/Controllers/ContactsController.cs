05375ff4d2c36148ae20e999ddf620466cadc07f
Mileeena <tgbgyukjh-fsdfsds@yahoo.com>
2022-07-10 17:17:00 +1000

    Вынес api в отдельный проект

---
+++ Phone_book/Phone_book.Api/Controllers/ContactsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Phone_book.Data.Models;
+using Phone_book.Data.Repository;
+
+namespace Phone_book.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private IRepository<Contact> _contextContacts;
+
+        private int UserId => int.Parse(User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+        public ContactsController(ILogger<ContactsController> logger, IRepository<Contact> contextContacts)
+        {
+            _contextContacts = contextContacts;
+        }
+
+        [HttpGet]
+        [Authorize (Roles = "User")]
+        [Route("all")]
+        public IActionResult GetAvailableContacts()
+        {
+            if (UserId != 1) return Ok(Enumerable.Empty<Contact>());
+
+            return Ok(_contextContacts.All);
+        }
+    }
+}
---
+++ Phone_book/Phone_book.Api/Startup.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Phone_book.Auth.Common;
+using Phone_book.Data;
+using Phone_book.Data.Models;
+using Phone_book.Data.Repository;
+
+namespace Phone_book.Api;
+
+public class Startup
+{
+    ///обратиться к appsettings
+    private readonly IConfiguration Configuration;
+
+    ///env - все инофрмация о приложении
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    /// добавление сервисов
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        var authOptions = Configuration.GetSection("Auth").Get<AuthOptions>();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = authOptions.Issuer,
+
+                    ValidateAudience = true,
+                    ValidAudience = authOptions.Audience,
+
+                    ValidateLifetime = true,
+
+                    IssuerSigningKey = authOptions.GetSymmetricSecurityKey(),
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+        });
+
+        services.AddMvc(options => options.EnableEndpointRouting = false);
+
+        string connectionString = Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<PhoneBookContext>(builder =>
+            builder.UseSqlServer(connectionString, b => b.MigrationsAssembly("Phone_book.Data"))
+        );
+        services.AddScoped<IRepository<Contact>, ContactRepository>();
+
+        //services.AddSwaggerGen(c =>
+        //{
+        //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        //    c.IncludeXmlComments(xmlPath);
+        //});
+    }
+    ///подключение контролеров, порядок важен
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        //app.UseSwagger();
+
+        //app.UseSwaggerUI(c =>
+        //{
+        //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        //});
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
